efa7c1a97052d8b7bc0e4ae41b1e6fd05f9c8de0
Timothy LaTorre <timlatorre@gmail.com>
2012-10-22 14:23:05 -0400

    finished lab1

---
+++ lab1/template.js
@@ -0,0 +1,48 @@
+// This is a single line comment
+
+/* This is a muli 
+line comment */
+
+$(function(){
+
+var first = prompt('What is your first name?');
+var last = prompt('What is your last name?');
+var full = first + " " + last;
+
+var age = prompt('What is your age?')
+age = parseInt(age);
+
+console.log('your full name is ' + full);
+console.log('your real age is ' + (age +10));
+
+if (age < 18)
+{
+	console.log('you are a minor');
+}
+else
+{
+	console.log('your are an adult');
+}
+
+if ((first == 'tim') && (last != 'smith'))
+	console.log('you are the winner');
+
+for(var i = 0; i < 10; i++)
+{
+	console.log('you are on number : ' + i);
+}
+
+var response = prompt('Enter a color or (q)uit?');
+
+var colors = [];
+
+while(response != 'q')
+{
+	colors.push(response);
+	console.log('you just typed in :' + response);
+	response = prompt('Enter a color or (q)uit');
+}
+
+console.log(colors); 
+
+});
